6dc84c30751eaa1c1d491cf3f3b233771ed7b798
Ruth <ruro122020@gmail.com>
2023-10-22 18:41:15 -0400

    setup useEffect to fetch data

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,12 @@ import React, {useState, useEffect} from 'react'
 const App = () => {
     const [isLoading, setIsLoading] = useState(false)
     const [image, setImage] = useState('')
+
+    useEffect(()=>{
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    })
   return (
     <div>
       
